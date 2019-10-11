1ddf718682e7a0a8018620fced39ff3f69670472
Hetarth Chopra <34271010+choprahetarth@users.noreply.github.com>
2019-10-12 02:28:22 +0530

    added start bit location finder
    code to be checked again tomorrow

--- serial1.py
+++ serial1.py
@@ -12,26 +12,56 @@ ser = serial.Serial(
 
 #this will store the line
 seq = []
+results =[]
 part = []
 count = 0
 cNew = 0 
 reserveBITCounter = 0
 queueChecker = 1
+position = 0 
+initMappingCounter = 0
+mappingCounter = 1
+checkingCounter = 1
+startLSBposition = 0
+startMSBposition = 0 
+startLSB2position = 0
+startMSB2position = 0
+
+def linearSearch(arr, x):
+    counter = 0  
+    while counter < len(arr):
+        if arr[counter] == x:
+            results.append(counter) 
+            counter = counter +1
+        else:
+            counter = counter +1
+    return results
+
+    
+
+def mapped():
+    if checkingCounter ==2:
+        print ("i am being called")
+        mappingCounter = 0
 
 while True:
     for c in ser.readline().hex():
-        if c == 'a' and queueChecker == 1:
-            reserveBITCounter = reserveBITCounter + 1
-        elif c != 'a' and queueChecker == 1:
-            reserveBITCounter = 0
-        if reserveBITCounter == 6:
-            queueChecker = 0
-            seq.append(c)
-            count += 1
-            if count == 38:
-                print (seq)
-                seq = []
-                count = 0 
-reserveBITCounter = 0 
-queueChecker = 1
+        seq.append(c)
+        count += 1
+        if count == 38:
+            linearSearch(seq,'a')
+            if len(results)==4 and checkingCounter==1:
+                initMappingCounter = initMappingCounter+1 
+            elif len(results)!=4 and checkingCounter==1:
+                initMappingCounter = 0
+            if (initMappingCounter > 4 and mappingCounter == 1):
+                checkingCounter = 2
+                print ("in here ")
+            print (results)
+            results = []
+            seq = []
+            count = 0 
 ser.close()
+
+
+    
